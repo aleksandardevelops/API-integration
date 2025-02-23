f031cd394295d388b363dfd27d06ae3e3853591d
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2025-02-23 19:37:20 +0100

    Hide image before user makes a search query

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function getSearch(event) {
   event.preventDefault();
   const query = searchBar.value;
 
+  if (query === '') {
+    img.style.display = 'none';
+  } else {
+    img.style.display = 'block'; // Ensure the image is shown when there is input
+  }
+
   fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=tatC4IzhmeaMqo83blsBNLEJmDj9qnzC&s=${query}`,
     { mode: 'cors' }
